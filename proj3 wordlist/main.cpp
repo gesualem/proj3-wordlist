899642ab67427e11650ee3bcc1717947a53d87d1
Michael <gesuale.m@husky.neu.edu>
2020-06-04 20:23:38 -0400

    Add project files.

---
+++ proj3 wordlist/main.cpp
@@ -0,0 +1,17 @@
+#include "wordlist.h"
+using namespace std;
+
+int main()
+{
+	Wordlist wordlist; 
+	wordlist.get_words();
+	wordlist.insertion_sort();
+
+	int result = wordlist.lookup();
+	if (result == -1)
+		cout << ("Element not present");
+	else
+		cout << ("Element found at index ") << result;
+
+}
+
